3b55fe6d9c73f7dae20358bfb0f6981a8c20ccf9
Giovanni Cerretani <g.cerretani@caen.it>
2024-06-14 12:25:27 +0200

    version to rc3

--- src/caen_libs/__init__.py
+++ src/caen_libs/__init__.py
@@ -8,5 +8,5 @@
 __author__ = 'Giovanni Cerretani'
 __copyright__ = 'Copyright (C) 2024 CAEN SpA'
 __license__ = 'LGPL-3.0-or-later'  # SPDX-License-Identifier
-__version__ = '1.0.0-rc2'
+__version__ = '1.0.0-rc3'
 __contact__ = 'https://www.caen.it/'
