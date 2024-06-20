0ccd9859191be6327352fb02e420de08afce5d98
Giovanni Cerretani <g.cerretani@caen.it>
2024-06-20 12:29:59 +0200

    update hvwrapper demo

--- tests/demo_hvwrapper.py
+++ tests/demo_hvwrapper.py
@@ -1,17 +1,31 @@
-from caen_libs import caenhvbinding as hv
+from caen_libs import caenhvwrapper as hv
 
 
 print(f'CAEN HV Wrapper binding loaded (lib version {hv.lib.sw_release()})')
 
-connection_type = hv.SystemType.N1470
-link_type = hv.LinkType.USB_VCP
+if False:
+    connection_type = hv.SystemType.SY4527
+    link_type = hv.LinkType.TCPIP
+    device = hv.Device.open(connection_type, link_type, '10.105.254.5', 'admin', 'admin')
+elif False:
+    connection_type = hv.SystemType.R6060
+    link_type = hv.LinkType.TCPIP
+    device = hv.Device.open(connection_type, link_type, '10.105.253.140', 'admin', 'admin')
+elif False:
+    connection_type = hv.SystemType.N1470
+    link_type = hv.LinkType.USB_VCP
+    device = hv.Device.open(connection_type, link_type, 'COM4_9600_8_1_0_0', '', '')
+else:
+    connection_type = hv.SystemType.V8100
+    link_type = hv.LinkType.TCPIP
+    device = hv.Device.open(connection_type, link_type, '10.105.254.13', '', '')
 
-with hv.Device.open(connection_type, link_type, 'COM4_9600_8_1_0_0', '', '') as device:
+with device:
 
     slots = device.get_crate_map()  # initialize internal stuff
 
-    comm_list = device.get_exec_comm_list()
-    print('EXEC_COMM_LIST', comm_list)
+    #comm_list = device.get_exec_comm_list()
+    #print('EXEC_COMM_LIST', comm_list)
 
     sys_props = device.get_sys_prop_list()
     for prop_name in sys_props:
@@ -23,24 +37,25 @@ with hv.Device.open(connection_type, link_type, 'COM4_9600_8_1_0_0', '', '') as 
             device.subscribe_system_params([prop_name])
 
     for slot, board in enumerate(slots):
-        if board.model != '':
-            bd_params = device.get_bd_param_info(slot)
-            for param_name in bd_params:
-                param_prop = device.get_bd_param_prop(slot, param_name)
-                print('BD_PARAM', slot, param_name, param_prop.type.name)
+        if board is None:
+            continue
+        bd_params = device.get_bd_param_info(slot)
+        for param_name in bd_params:
+            param_prop = device.get_bd_param_prop(slot, param_name)
+            print('BD_PARAM', slot, param_name, param_prop.type.name)
+            if param_prop.mode != hv.ParamMode.WRONLY:
+                param_value = device.get_bd_param([slot, slot], param_name)
+                print('VALUE', param_value)
+                device.subscribe_board_params(slot, [param_name])
+        for ch in range(board.n_channel):
+            ch_params = device.get_ch_param_info(slot, ch)
+            for param_name in ch_params:
+                param_prop = device.get_ch_param_prop(slot, ch, param_name)
+                print('CH_PARAM', slot, ch, param_name, param_prop.type.name)
                 if param_prop.mode != hv.ParamMode.WRONLY:
-                    param_value = device.get_bd_param([slot], param_name)
+                    param_value = device.get_ch_param(slot, [ch], param_name)
                     print('VALUE', param_value)
-                    device.subscribe_board_params(slot, [param_name])
-            for ch in range(board.n_channel):
-                ch_params = device.get_ch_param_info(slot, ch)
-                for param_name in ch_params:
-                    param_prop = device.get_ch_param_prop(slot, ch, param_name)
-                    print('CH_PARAM', slot, ch, param_name, param_prop.type.name)
-                    if param_prop.mode != hv.ParamMode.WRONLY:
-                        param_value = device.get_ch_param(slot, [ch], param_name)
-                        print('VALUE', param_value)
-                        device.subscribe_channel_params(slot, ch, [param_name])
+                    device.subscribe_channel_params(slot, ch, [param_name])
 
     while True:
         evt_list, _ = device.get_event_data()
