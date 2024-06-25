579d2a58bb09262adac1cede749f59a1caa38aea
Giovanni Cerretani <g.cerretani@caen.it>
2024-06-25 10:45:42 +0200

    version to 1.0.1. add changelog

--- src/caen_libs/__init__.py
+++ src/caen_libs/__init__.py
@@ -9,5 +9,5 @@ __author__ = 'Giovanni Cerretani'
 __copyright__ = 'Copyright (C) 2024 CAEN SpA'
 __license__ = 'LGPL-3.0-or-later'
 # SPDX-License-Identifier: LGPL-3.0-or-later
-__version__ = '1.0.0'
+__version__ = '1.0.1'
 __contact__ = 'https://www.caen.it/'
